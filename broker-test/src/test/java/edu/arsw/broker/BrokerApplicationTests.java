939c79857881cbbc7642b8ea59933c195b48b0b1
Andr3xDev <achavarropp@gmail.com>
2025-07-26 03:32:37 -0500

    add: message brocker example

---
+++ broker-test/src/test/java/edu/arsw/broker/BrokerApplicationTests.java
@@ -0,0 +1,12 @@
+package edu.arsw.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class BrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
