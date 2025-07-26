ec551e406e0bf1f2b02efb8facce15db25c2ef96
Andr3xDev <achavarropp@gmail.com>
2025-07-26 03:17:05 -0500

    add: initial react intro

---
+++ intro-react/src/test/java/edu/arsw/react/ReactApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.arsw.react;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class ReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
